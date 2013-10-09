0a6c9e381770b2ded98a8b12ce19830b998656c1
Charles Nordine <cgnemail-spam@yahoo.com>
2013-10-08 22:54:05 -0400

    added contacts, modals and some screen flow

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,3 +1,11 @@
 
 // steroids.view.navigationBar.show("Hello World");
+function showModal(modal) {
+  var webView = new steroids.views.WebView(modal);
+  steroids.modal.show(webView);
+}
 
+function previewDolanImage() {
+  var fileView = new steroids.views.PreviewFileView("images/dolan.png");
+  steroids.modal.show(fileView);
+}
