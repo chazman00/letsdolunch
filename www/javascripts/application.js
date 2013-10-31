f7c694adf2a32fb03505a58e394b7b6c0a68bcc9
Charles Nordine <cgnemail-spam@yahoo.com>
2013-10-30 21:27:57 -0400

    added jquery mobile

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -76,64 +76,62 @@ function showModal(modal) {
 function onDeviceReady() {
         // find all contacts with 'Bob' in any name field
         var options = new ContactFindOptions();
-        options.filter="Peter"; 
-        var fields = ["displayName", "name", "nickname", "emails"];
-        //navigator.contacts.find(fields, onContactSuccess, onContactError, options);
-    }
-	
-	function textChanged(elm)
-	{
-		var options = new ContactFindOptions();
-        options.filter=elm.value; 
-		options.multiple=true;
+        options.filter="Rachel"; 
+		//options.multiple=true;
         var fields = ["displayName", "name", "nickname", "emails"];
         navigator.contacts.find(fields, onContactSuccess, onContactError, options);
+    }
+	
+	function textChanged(value)
+	{
+		var options = new ContactFindOptions();
+        options.filter=value; 
+		options.multiple=true;
+        var fields = ["displayName", "name", "nickname", "emails"];
+        //navigator.contacts.find(fields, onContactSuccess, onContactError, options);
 	}
 
     // onSuccess: Get a snapshot of the current contacts
     //
     function onContactSuccess(contacts) {
-	
+		var html = "";
+		var ul = $( "#autocomplete" );
+		$.each( contacts, function ( i,contact ) {
+			var emailHTML = "";
+			$.each(contact.emails, function (j, email) {
+				emailHTML += email.value + "<br/>";
+			});
+			html += "<li><a href='#'><h2>" +contact.name.formatted + "</h2><p>"+ emailHTML +"</p></a></li>";
+		});
+		ul.html( html );
+		ul.listview( "refresh" );
+		ul.trigger( "updatelayout");
 		
 		/*
-		 List
-		 ------------
+		
 
-			<div class="topcoat-list__container">
-			  <h3 class="topcoat-list__header">Category</h3>
-			  <ul class="topcoat-list">
-				<li class="topcoat-list__item">
-				  Item
-				</li>
-				<li class="topcoat-list__item">
-				  Item
-				</li>
-				<li class="topcoat-list__item">
-				  Item
-				</li>
-			  </ul>
-			</div>
-
-		*/
+		
 		var searchDiv = document.getElementById("contactsSearch");
 		var divCont = document.createElement("div");
 		//divCont.setAttribute("class","topcoat-list__container");
 		var list = document.createElement("ul");
-		//list.setAttribute("class","topcoat-list");	
+		list.setAttribute("data-role","listview");	
 		searchDiv.appendChild(divCont);
 		divCont.appendChild(list);
-        for (var i=0; i<contacts.length; i++) {					
+        for (var i=0; i<contacts.length; i++) {		
+			$("#contactsSearch").append("<li><a href='#'>" + contacts[i].name.formatted + "</a></li>");
+			
 			var listItem = document.createElement("li");
-			//listItem.setAttribute("class","topcoat-list__item");		
+			listItem.setAttribute("class","topcoat-list__item");		
 			listItem.setAttribute("id","contact"+i);	
 			//listItem.setAttribute("ontouchend","submitContacts('this')");
-            var txt = document.createTextNode(contacts[i].name.formatted);
+            var txt = document.createTextNode();
 			listItem.appendChild(txt);
 			list.appendChild(listItem);
-
+			*/
 			
-        }
     }
+    
 	
 	function submitContacts(element) {
 		//var data = $("#datadiv");
