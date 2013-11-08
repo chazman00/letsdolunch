c78623f68663c84a810e136eabcb17b1bf2b18c7
rktechnophile <richard.krott@gmail.com>
2013-11-08 17:59:30 -0500

    Sample files for testing the data model capabilities of the StackMob platform.

---
+++ www/javascripts/data.js
@@ -0,0 +1,23 @@
+var Event = StackMob.Model.extend({
+	schemaName: 'event'
+});
+
+var Events = StackMob.Collection.extend({
+	model: Event
+});
+
+var Contact = StackMob.Model.extend({
+	schemaName: 'contact'
+});
+
+var Location = StackMob.Model.extend({
+	schemaName: 'location'
+});
+
+var EventContactPreferences = StackMob.Model.extend({
+	schemaName: 'eventcontactpreferences'
+});
+
+var EventLocationFeedback = StackMob.Model.extend({
+	schemaName: 'eventlocationfeedback'
+});
